4d88785ee4f156287ac9897bf40a3a93d2c7b3cf
golnar-mh <crypgirlmh@gmail.com>
2025-05-12 19:25:06 +0300

    feat(cdc): code transaction to the chain

--- x/pool/types/codec.go
+++ x/pool/types/codec.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 var (
@@ -18,8 +19,7 @@ func init() {
 }
 
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
-	// cdc.RegisterConcrete(&MsgDistributeAuctionReward{}, "gemmint/MsgDistributeAuctionReward", nil)
-	// cdc.RegisterConcrete(&MsgMintRedeemedOptionNFT{}, "gemmint/MsgMintRedeemedOptionNFT", nil)
+	cdc.RegisterConcrete(&MsgMintTribute{}, "pool/MsgMintTribute", nil)
 	// // this line is used by starport scaffolding # 2
 
 	// // proposals
@@ -28,13 +28,12 @@ func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 
-	// registry.RegisterImplementations((*sdk.Msg)(nil),
-	// 	&MsgDistributeAuctionReward{},
-	// 	&MsgMintRedeemedOptionNFT{},
-	// )
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgMintTribute{},
+	)
 	// this line is used by starport scaffolding # 3
 
-	// msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 
 	// proposals
 	// registry.RegisterImplementations(
