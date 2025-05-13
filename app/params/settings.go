d4a7e79ac91251066e06d72b45757f83dd922a49
golnar-mh <crypgirlmh@gmail.com>
2025-05-13 14:31:46 +0300

    refactor: rename denom from qnt to outbe

---
+++ app/params/settings.go
@@ -0,0 +1,14 @@
+package params
+
+const (
+	AccountAddressPrefix = "outbe"
+	Name                 = "outbe-node"
+
+	// BondDenom defines the native staking token denomination.
+	BondDenom = "outbe"
+
+	MinDeposit = 10000000
+
+	Prefix     = "q"
+	TurnUpLogs = "true"
+)
