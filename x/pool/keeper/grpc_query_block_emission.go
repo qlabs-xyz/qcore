41e7aee41bba483356d062a249ec389f1b162210
golnar-mh <crypgirlmh@gmail.com>
2025-05-13 14:45:24 +0300

    feat: impelemnt a query for fetching emission amount

---
+++ x/pool/keeper/grpc_query_block_emission.go
@@ -0,0 +1,39 @@
+package keeper
+
+import (
+	"context"
+	"errors"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/outbe/outbe-node/x/pool/constants"
+	"github.com/outbe/outbe-node/x/pool/types"
+)
+
+func (k Keeper) GetBlockEmission(goCtx context.Context, req *types.QueryBlockEmissionRequest) (*types.QueryBlockEmissionResponse, error) {
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	if req == nil {
+		return nil, errors.New("[GetBlockEmission] failed. request is nil")
+	}
+
+	if ctx.BlockHeight() < constants.TransitionBlockNumber {
+
+		if req.BlockNumber < 0 {
+			return nil, errors.New("blocknumber is 0")
+		}
+
+		tokens, err := k.CalculateExponentialBlockEmission(req.BlockNumber)
+		if err != nil {
+			return nil, errors.New("[GetBlockEmission][CalculateExponentialBlockEmission] failed.CalculateExponentialTokens failed")
+		}
+		return &types.QueryBlockEmissionResponse{Tokens: tokens}, nil
+	}
+
+	tokens, err := k.CalculateFixedBlockEmission(goCtx)
+	if err != nil {
+		return nil, errors.New("[GetBlockEmission][CalculateFixedBlockEmission] failed. CalculateFixedTokens failed")
+	}
+
+	return &types.QueryBlockEmissionResponse{Tokens: tokens}, nil
+}
